ea0f1e6301bb902baa6e140aa99d321cdb77e7d9
Roman Miniv <1pi15bminivroman@gmail.com>
2019-08-05 22:34:53 +0300

    user page: code review

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Switch, Route, Redirect } from 'react-router-dom';
-import "./styles/App.scss";
-
 import User from './components/UserPage/UserPage';
-
+import "./styles/App.scss";
 
 const App: React.FC = () => {
   return (
