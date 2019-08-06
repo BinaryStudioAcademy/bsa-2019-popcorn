01e18bc85ea94e215d85e4f8276ae356a8e0d5ef
Taras <tarasnazar93@gmail.com>
2019-08-06 13:08:11 +0300

    feed: added variables

--- client/src/components/FeedBlock/FeedBlock.tsx
+++ client/src/components/FeedBlock/FeedBlock.tsx
@@ -3,7 +3,7 @@ import FeedList from "../FeedList/FeedList"
 import RecommendList from "../RecommendList/RecommendList"
 import "./FeedBlock.scss"
 
-const FeedBlock=()=>{
+const FeedBlock =()=>{
         return <div className="feed-block">
             <FeedList />
             <RecommendList />
