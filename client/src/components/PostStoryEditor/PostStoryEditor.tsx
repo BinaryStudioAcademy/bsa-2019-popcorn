c1bb7a9c0171a07ee626887e7781cb2557f38c01
prikhodkoos <prikhodks@gmail.com>
2019-08-06 15:41:52 +0300

    post-story-editor: added input to edit post/story body

--- client/src/components/PostStoryEditor/PostStoryEditor.tsx
+++ client/src/components/PostStoryEditor/PostStoryEditor.tsx
@@ -1,14 +1,67 @@
 import React from "react";
 
 interface IPostStoryEditorProps {
-  
+  post_id?: string,
+  story_id?: string
 }
-interface IPostStoryEditorState {
 
+interface IPostStoryEditorState {
+  image?: string,
+  body: string
 }
 
 class PostStoryEditor extends React.Component<IPostStoryEditorProps, IPostStoryEditorState> {
+  constructor(props: IPostStoryEditorProps) {
+    super(props);
+    this.state = {
+      image: undefined,
+      body: ''
+    };
 
+    this.onCancel = this.onCancel.bind(this);
+    this.onSave = this.onSave.bind(this);
+    this.onChangeData = this.onChangeData.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.props.post_id) {
+      // fetch post by id
+      this.setState({
+        ...this.state,
+        body: 'test post'
+      });
+    }
+    if (this.props.story_id) {
+      // fetch story by id
+      this.setState({
+        ...this.state,
+        body: 'test story'
+      });
+    }
+  }
+
+  onChangeData(e: React.FormEvent<HTMLInputElement>, keyword: string) {
+    const target = e.target as HTMLTextAreaElement;
+    const value = target.value;
+    this.setState({
+        ...this.state,
+        [keyword]: value
+    });
+  }
+
+  onCancel() {
+  }
+
+  onSave() {
+  }
+
+  render() {
+    return (
+      <div>
+        <input type='text' value={this.state.body} onChange={e => this.onChangeData(e, 'body')}/>
+      </div>
+    );
+  }
 }
 
 export default PostStoryEditor;
