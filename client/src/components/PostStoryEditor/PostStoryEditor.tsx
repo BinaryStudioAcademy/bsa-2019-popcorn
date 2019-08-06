9aa6d72004d5d8a8c21280de5772af8bdea81799
prikhodkoos <prikhodks@gmail.com>
2019-08-06 14:11:48 +0300

    post-story-editor: created PostStoryEditor component folder

---
+++ client/src/components/PostStoryEditor/PostStoryEditor.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface IPostStoryEditorProps {
+  
+}
+interface IPostStoryEditorState {
+
+}
+
+class PostStoryEditor extends React.Component<IPostStoryEditorProps, IPostStoryEditorState> {
+
+}
+
+export default PostStoryEditor;
