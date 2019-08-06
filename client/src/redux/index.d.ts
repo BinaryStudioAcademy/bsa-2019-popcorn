23c571108b5edb6053faa1e637cb646d02233e9a
Алексей Тряпко <tryapko@icloud.com>
2019-08-06 14:54:50 +0300

    Revert "Feature/redux"

--- client/src/redux/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "redux-saga-routines"
-
-interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-}
-
-function combineReducers<S>(reducers: ReducersMapObject): Reducer<S>;
