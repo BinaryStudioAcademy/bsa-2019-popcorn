b8a5bd58ad783750106f428e913552baa2586602
Taras <tarasnazar93@gmail.com>
2019-08-06 16:50:15 +0300

    feed: fix merge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./styles/App.scss";
-import FeedBlock from "./components/FeedBlock/FeedBlock"
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <FeedBlock/>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 };
