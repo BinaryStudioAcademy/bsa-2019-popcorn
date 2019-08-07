9dd18d134919c8ac7beb8b429ca0a6cc6925dc73
Alexey Tryapko <aatryapko1201@gmail.com>
2019-08-07 21:08:25 +0300

    set-up-routing: connect components

--- client/src/components/MovieSeriesPage/MovieSeriesPageTabBody.tsx
+++ client/src/components/MovieSeriesPage/MovieSeriesPageTabBody.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
-import MovieSeriesBasic from '../MovieSeriesBasic/MovieSeriesBasic';
+import DiscussionComponent from '../DiscussionComponent/DiscussionComponent';
+import FilmBasicTabComponent from '../FilmBasicTabComponent/FilmBasicTabComponent';
 import MovieSeriesCastCrew from '../MovieSeriesCastCrew/MovieSeriesCastCrew';
 import MovieSeriesReviews from '../MovieSeriesReviews/MovieSeriesReviews';
 import MovieSeriesPosts from '../MovieSeriesPosts/MovieSeriesPosts';
 import MovieSeriesAwards from '../MovieSeriesAwards/MovieSeriesAwards';
 import MovieSeriesStatistics from '../MovieSeriesStatistics/MovieSeriesStatistics';
+
+const messages =
+        [{
+            id: '1',
+            name: 'Morris',
+            body: 'Nice to see you',
+            photo: 'https://img.icons8.com/dusk/64/000000/circled-user-male-skin-type-6.png',
+            date: '08.12.2017 12:35'
+        }];
 
 interface IProps {
     mainPath: string
@@ -16,13 +25,14 @@ const MovieSeriesPageTabBody: React.SFC<IProps> = ({ mainPath }) => {
     return (
         <div className="movie-series-page-tab-body">
             <Switch>
-                <Route exact path={`${mainPath}`} component={MovieSeriesBasic} />
+                <Route exact path={`${mainPath}`} component={FilmBasicTabComponent} />
                 <Route path={`${mainPath}/cast-crew`} component={MovieSeriesCastCrew} />
                 <Route path={`${mainPath}/reviews`} component={MovieSeriesReviews} />
                 <Route path={`${mainPath}/posts`} component={MovieSeriesPosts} />
                 <Route path={`${mainPath}/awards`} component={MovieSeriesAwards} />
                 <Route path={`${mainPath}/statistics`} component={MovieSeriesStatistics} />
             </Switch>
+            <DiscussionComponent messages={messages}/>
         </div>
     );
 }
