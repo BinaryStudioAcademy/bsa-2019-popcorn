9f8d6b44821fac7b20e5edcd83a7f6034f98946f
Kirill Burmelov <burik0304@icloud.com>
2019-08-07 15:23:08 +0300

    post-api: create router api/post

--- server/controllers/root.controller.ts
+++ server/controllers/root.controller.ts
@@ -1,10 +1,12 @@
 import authRoutes from './auth.controller';
 import imageRouters from "./image.controller";
 import movieRouter from './movie.controller';
+import postRouter from './post.controller';
 
 // register all routes
 export default (app) => {
     app.use('/api/auth', authRoutes);
     app.use('/image', imageRouters)
     app.use('/api/movie', movieRouter);
+    app.use('/api/post', postRouter);
 };
