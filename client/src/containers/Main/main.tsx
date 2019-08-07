ca236a2d37bbb6286a597049a0b7996f3b81ad2b
Alexey Tryapko <aatryapko1201@gmail.com>
2019-08-07 21:07:38 +0300

    set-up-routing: add container for main page

---
+++ client/src/containers/Main/main.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import MainPage from "./../../components/MainPage/MainPage";
+import UserPage from "./../../components/UserPage/UserPage";
+import MovieSeriesPage from "./../../components/MovieSeriesPage/MovieSeriesPage";
+import MainPageSidebar from "./../../components/MainPageSidebar/MainPageSidebar";
+import MovieList from './../../components/MovieList/MovieList';
+import ProfileComponent from './../../components/ProfileComponent/ProfileComponent';
+import "./MainContainer.scss";
+
+const { userInfo, notifications } = {
+    userInfo: {
+        name: "Sofi Dub",
+        image: "https://s3-alpha-sig.figma.com/img/919e/1a5a/da4f250d469108191ad9d4af68b2a639?Expires=1566172800&Signature=Kou41Z8bd8ig~9nLibgCH5gfaOc0K~9Io82-umabjJnomveXbPcqMWfD911bHy6h77reHT6ecNYFHCzmXkQNy3vEF-OzgJYgV875TI2rX~cPt1FaSJC5wCeybEfTrlBlCcdzSFn8iVcP~C8GTx-l6CIjyugGAhvr7xJ-hfAdlf~5Mll0Sy92dSKn8q7OkJdfsMvEEFVQ3rGHn8GGQZg1a60gif0VaQhuVX1gcRgwrsak~cerS1bnDvo93B1lFOIk85wlhY2hPwQrmCtI9A-qaAtbIxmzmxkRpuVUpDrX6Jd4hXpksbd7urSJ91Dg7tv9WzRZvIkLnPXflCfmPw~slw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+    },
+    notifications: {
+        newFriends: 12,
+        newMessages: 0,
+        newEvents: 2
+    }
+}
+
+const profileInfo = {
+    name: "Sofi Dub",
+    male: false,
+    female: true,
+    location: "KIev",
+    about: "Give me films",
+    photoSvg:  "https://s3-alpha-sig.figma.com/img/919e/1a5a/da4f250d469108191ad9d4af68b2a639?Expires=1566172800&Signature=Kou41Z8bd8ig~9nLibgCH5gfaOc0K~9Io82-umabjJnomveXbPcqMWfD911bHy6h77reHT6ecNYFHCzmXkQNy3vEF-OzgJYgV875TI2rX~cPt1FaSJC5wCeybEfTrlBlCcdzSFn8iVcP~C8GTx-l6CIjyugGAhvr7xJ-hfAdlf~5Mll0Sy92dSKn8q7OkJdfsMvEEFVQ3rGHn8GGQZg1a60gif0VaQhuVX1gcRgwrsak~cerS1bnDvo93B1lFOIk85wlhY2hPwQrmCtI9A-qaAtbIxmzmxkRpuVUpDrX6Jd4hXpksbd7urSJ91Dg7tv9WzRZvIkLnPXflCfmPw~slw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+}
+
+const movies = [
+    {
+      id: '8c0bb20f-fc81-473b-8a73-5ae2125fe603', 
+      title: 'Titanic',
+      releaseDate: new Date(),
+      image: 'https://images-na.ssl-images-amazon.com/images/I/51gEpO63aRL.jpg',
+      duration: '3h 21min',
+      genres: ['Drama', 'Action', 'Family'],
+      cast: ['leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio']
+    },
+    {
+      id: '8c0bb1ef-fc81-473b-8a73-5ae2125fe603', 
+      title: 'Forrest Gump',
+      releaseDate: new Date(),
+      image: 'https://posteritati.com/posters/000/000/053/106/forrest-gump-md-web.jpg',
+      duration: '1h 33min',
+      genres: ['Drama'],
+      cast: ['leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio']
+    },
+    {
+      id: '8c0bb20f-fc81-423b-8a73-5ae2125fe603', 
+      title: 'Titanic1',
+      releaseDate: new Date(),
+      image: 'https://images-na.ssl-images-amazon.com/images/I/51gEpO63aRL.jpg',
+      duration: '3h 21min',
+      genres: ['Drama', 'Action', 'Family'],
+      cast: ['leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio']
+    },
+    {
+      id: '8c0bb1ef-fc11-473b-8a73-5ae2125fe603', 
+      title: 'Forrest Gump12',
+      releaseDate: new Date(),
+      image: 'https://posteritati.com/posters/000/000/053/106/forrest-gump-md-web.jpg',
+      duration: '1h 13min',
+      genres: ['Drama'],
+      cast: ['leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio']
+    },
+    {
+      id: '8c0bb20f-fc81-473b-8a73-5ae2121fe603', 
+      title: 'Titanic',
+      releaseDate: new Date(),
+      image: 'https://images-na.ssl-images-amazon.com/images/I/51gEpO63aRL.jpg',
+      duration: '3h 21min',
+      genres: ['Drama', 'Action', 'Family'],
+      cast: ['leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio']
+    },
+    {
+      id: '8c0bb1ef-fc81-473b-8a73-5ae2125fe60a', 
+      title: 'Forrest Gump',
+      releaseDate: new Date(),
+      image: 'https://posteritati.com/posters/000/000/053/106/forrest-gump-md-web.jpg',
+      duration: '1h 33min',
+      genres: ['Drama'],
+      cast: ['leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio', 'leonardo dicaprio']
+    }
+  ];
+
+const Main = ({ match }) => {
+    const { path } = match;
+    return (
+        <div className="main-page">
+            <MainPageSidebar userInfo={userInfo} notifications={notifications} />
+            <div>
+                <Switch>
+                    <Route exact path={`${path}`} component={MainPage} />
+                    <Route path={`${path}/user-page`} component={UserPage} />
+                    <Route path={`${path}/movie-series`} component={MovieSeriesPage} />
+                    <Route path={`${path}/movie-list`} render={() => <MovieList movies={movies}/>}/>
+                    <Route path={`${path}/user-profile`} render={() => <ProfileComponent profileInfo={profileInfo}/>}/>
+                </Switch>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
--- client/src/containers/Routing/routing.tsx
+++ client/src/containers/Routing/routing.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from "./../../components/Login/Login";
 import Registration from "./../../components/Registration/index";
-import MainPage from "./../../components/MainPage/MainPage";
+import Header from "./../../components/Header/Header";
+import Main from "./../Main/main";
+
 const Routing = () => {
     return (
-        <Switch>
-            <Route exact path="/login" component={Login}/>
-            <Route exact path="/registration" component={Registration}/>
-            <Route exact path="/" component={MainPage}/>
-            {/* Not found route */}
-            <Route component={MainPage}/>
-        </Switch>
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/registration" component={Registration} />
+                <Route path="/app" component={Main} />
+                {/* Not found route */}
+                {/* <Route component={NotFound} /> */}
+            </Switch>
+        </div>
+
     );
 };
 
