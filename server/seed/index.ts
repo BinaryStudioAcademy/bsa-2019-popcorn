55dc8d0aaee6df7bb539923b99a5d8f9b531a0c8
igor185 <igor.babin185@gmail.com>
2019-08-07 11:38:02 +0300

    migration-data: set up migration

---
+++ server/seed/index.ts
@@ -0,0 +1,6 @@
+import {SeedUsers1565158741120} from './1565158741120-SeedUsers';
+
+//to create new migration run in this folder: typeorm migration:create -n <name>
+// then import this migration hear and export in array below
+
+export default [SeedUsers1565158741120];
