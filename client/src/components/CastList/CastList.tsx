8b03c71c832af7161a5e5dedf335fedb9b2ca9eb
Taras <tarasnazar93@gmail.com>
2019-08-07 17:52:46 +0300

    crew/cast:  mocked data stored in array

--- client/src/components/CastList/CastList.tsx
+++ client/src/components/CastList/CastList.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import CastItem from "../CastItem/CastItem"
 import './CastList.scss'
 
-type CastListProps={
+type CastListProps = {
 
 }
 
-const CastList = ()=>{
+const castListMock = [{ name: 'Viggo Mortensen', image: 'https://pixel.nymag.com/imgs/fashion/daily/2016/05/25/25-Viggo-Mortensen.w700.h700.jpg', role: 'Tony Lip' },
+{ name: 'Mahershala Ali', image: 'https://pixel.nymag.com/imgs/daily/vulture/2018/12/04/04-mahershala-ali.w330.h330.jpg', role: 'Dr. Donald Shirley' },
+{ name: 'Linda Cardellini', image: 'https://pbs.twimg.com/profile_images/1016413503081463808/yikT0nAV.jpg', role: 'Dolores' },
+{ name: 'Dimiter D. Marinov', image: 'https://img.reelgood.com/content/person/3bd2fb39-93a0-40f4-a221-755c6842e7dc/square-242.jpg', role: 'Oleg' }];
+
+const CastList = () => {
     return <div className='cast-list'>
         <div className='cast-list-header'>Main cast</div>
         <div className='cast-items-container'>
-        <CastItem castItemInfo={{name:'Viggo Mortensen', image:'https://pixel.nymag.com/imgs/fashion/daily/2016/05/25/25-Viggo-Mortensen.w700.h700.jpg', role:'Tony Lip'}}/>
-        <CastItem castItemInfo={{name:'Mahershala Ali', image:'https://pixel.nymag.com/imgs/daily/vulture/2018/12/04/04-mahershala-ali.w330.h330.jpg', role:'Dr. Donald Shirley'}}/>
-        <CastItem castItemInfo={{name:'Linda Cardellini', image:'https://pbs.twimg.com/profile_images/1016413503081463808/yikT0nAV.jpg', role:'Dolores'}}/>
-        <CastItem castItemInfo={{name:'Dimiter D. Marinov', image:'https://img.reelgood.com/content/person/3bd2fb39-93a0-40f4-a221-755c6842e7dc/square-242.jpg', role:'Oleg'}}/>
+            {
+            castListMock.map(el=>{return <CastItem castItemInfo={el}/>})
+            }
         </div>
     </div>
 }
--- client/src/components/StaffList/StaffList.tsx
+++ client/src/components/StaffList/StaffList.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import StaffItem from '../StaffItem/StaffItem';
 import './StaffList.scss';
 
-const StaffList = ()=>{
+type StaffListProps = {
+
+}
+
+const staffListMock = [{ names: ['Pete Farrelly'], role: 'Director' },
+{ names: [' Nick Vallelonga', 'Brian Hayes Currie', 'Peter Farrelly'], role: 'Writers' },
+{ names: ['Pete Farrelly'], role: 'Music by' },
+{ names: ['Universal Pictures', 'DreamWorks'], role: 'Studios' }]
+const StaffList = () => {
     return <div className='staff-list'>
-         <div className='staff-list-header'>Staff</div>
-         <div className='staff-items-container'>
-             <StaffItem staffItemInfo={{names:['Pete Farrelly'], role:'Director'}}></StaffItem>
-             <StaffItem staffItemInfo={{names:[' Nick Vallelonga', 'Brian Hayes Currie', 'Peter Farrelly'], role:'Writers'}}></StaffItem>
-             <StaffItem staffItemInfo={{names:['Pete Farrelly'], role:'Music by'}}></StaffItem>
-             <StaffItem staffItemInfo={{names:['Universal Pictures', 'DreamWorks'], role:'Studios'}}></StaffItem>
-         </div>
+        <div className='staff-list-header'>Staff</div>
+        <div className='staff-items-container'>
+            {staffListMock.map(el=>{return <StaffItem staffItemInfo={el}></StaffItem>})}
+        </div>
     </div>
 }
 
