8e0468e526a8e736f7d96f3bd347b1dd1c4cced7
Kirill Burmelov <burik0304@icloud.com>
2019-08-08 02:43:27 +0300

    post-api: add get and post requests

--- server/controllers/post.controller.ts
+++ server/controllers/post.controller.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
-import * as postService from '../services/post.service'
+import * as postService from '../services/post.service';
+import { Post } from '../models/PostModel';
 
 const router = Router();
 
 router
   .get('/', (req, res, next) => postService.getPosts()
-    .then((posts) => {
-      console.log('success');
-      console.log(posts);
-      res.send(posts);
-    })
+    .then( (posts: Post[]) => { res.send(posts) })
+    .catch(next))
+  .post('/', (req, res, next) => postService.createPost(req.body)
+    .then( (response) => res.send(response) )
     .catch(next))
 
 export default router;
--- server/services/post.service.ts
+++ server/services/post.service.ts
@@ -1,14 +1,14 @@
-import {Post} from '../models/PostModel';
+import { Post } from '../models/PostModel';
 import PostRepository from '../repository/post.repository';
-import {getRepository, getCustomRepository, AdvancedConsoleLogger} from "typeorm";
+import { getRepository, getCustomRepository } from "typeorm";
 
-export const createPost = async (post: any): Promise<Post> =>
-    await getRepository(PostRepository)
+export const createPost = async (post: Post): Promise<Post> => 
+    await getCustomRepository(PostRepository)
         .save(post);
 
-export const getPosts = async (): Promise<Post[]> => {
-    return await getRepository(Post)
-        .createQueryBuilder("Post")
-        .leftJoinAndSelect("Post.userId", "id")
-        .getMany();
-}
+    
+export const getPosts = async (): Promise<Post[]> =>
+    await getCustomRepository(PostRepository)
+        .createQueryBuilder('post')
+        .leftJoinAndSelect('post.user', 'user')
+        .getMany()
