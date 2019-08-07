80c719645c12418cad3a4530682d1ee4c1185d4b
alex_yavor <alex.yavor1@gmail.com>
2019-08-07 13:55:28 +0300

    routers: add imageRouter to export

--- server/routers/index.ts
+++ server/routers/index.ts
@@ -1,4 +1,6 @@
-import movieRoutes from './movie.router';
+import movieRouter from './movie.router';
+import imageRouter from './image.router';
 export default (app) => {
-    app.use('/api/movie', movieRoutes);
+    app.use('/api/movie', movieRouter);
+    app.use('/api/image', imageRouter);
 };
