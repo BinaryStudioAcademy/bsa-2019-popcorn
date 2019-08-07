326539e1cee6d1687554291d86f735b3981e052f
Alexey Tryapko <aatryapko1201@gmail.com>
2019-08-07 17:50:36 +0300

    set-up-routing: inject routing to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,22 @@
 import React from "react";
-import { Switch, Route, Redirect } from 'react-router-dom';
-import UserPage from './components/UserPage/UserPage';
-import MovieSeriesPage from './components/MovieSeriesPage/MovieSeriesPage';
 import "./styles/App.scss";
-import FilmBasicTab from "./components/FilmBasicTabComponent/FilmBasicTabComponent";
+import Routing from "./containers/Routing/routing";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <FilmBasicTab />
-      <Switch>
-        <Route path="/user" component={UserPage} />
-        <Route path="/movie-series-page" component={MovieSeriesPage} />
-        <Redirect to="/" />
-      </Switch>
-    </div>
+    <Router>
+      <Routing />
+    </Router>
   );
 };
 
 export default App;
+{/* <div className="App">
+<FilmBasicTab />
+<Switch>
+  <Route path="/user" component={UserPage} />
+  <Route path="/movie-series-page" component={MovieSeriesPage} />
+  <Redirect to="/" />
+</Switch>
+</div> */}
