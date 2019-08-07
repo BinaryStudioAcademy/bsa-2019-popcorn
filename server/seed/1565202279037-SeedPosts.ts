9933fc1fde478660889336f65a9c35c1fd287f45
Kirill Burmelov <burik0304@icloud.com>
2019-08-07 23:01:27 +0300

    post-api: fix migration

--- server/seed/1565202279037-SeedPosts.ts
+++ server/seed/1565202279037-SeedPosts.ts
@@ -1,24 +1,21 @@
 import { MigrationInterface, QueryRunner, getCustomRepository } from "typeorm";
 import PostRepository from "../repository/post.repository";
+import UserRepository from "../repository/user.repository";
 import { Post } from "../models/PostModel";
 const uuid = require('uuid/v4');
-
 export class SeedPosts1565202279037 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        
-        const post = new Post();
-        post.id = uuid();
-        post.title = 'title-example';
-        post.description = 'description-example';
-        post.image_url = 'image-url-example';
-        post.userId = "1cac6cbb-ff54-47fd-852e-991eb264ee44";
+   public async up(queryRunner: QueryRunner): Promise<any> {
 
-        await getCustomRepository(PostRepository)
-            .save(post);
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<any> {
-    }
-
+       const post = new Post();
+       post.id = uuid();
+       post.title = 'title-example';
+       post.description = 'description-example';
+       post.image_url = 'image-url-example';
+       post.user = await getCustomRepository(UserRepository).findOne("7f13634d-c353-433c-98fe-ead99e1252c7");
+       await getCustomRepository(PostRepository)
+           .save(post);
+   }
+   public async down(queryRunner: QueryRunner): Promise<any> {
+   }
 }
