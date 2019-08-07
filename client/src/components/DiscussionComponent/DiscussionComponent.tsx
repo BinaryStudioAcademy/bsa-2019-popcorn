283e235bad0861ea349ae7bd45c364cd1dff41af
Vera Shreder <v.shreder@lenal.biz>
2019-08-07 10:37:35 +0300

    add discussion component:fix

--- client/src/components/DiscussionComponent/DiscussionComponent.tsx
+++ client/src/components/DiscussionComponent/DiscussionComponent.tsx
@@ -2,55 +2,52 @@ import React, { Component, createRef } from 'react';
 import "./DiscussionComponent.scss";
 
 interface IDiscussionProps {
-	messages: {id: string,
+	messages: {
+		id: string,
 		name: string,
 		body: string,
 		photo: string,
-		date: string}[];
+		date: string
+	}[]
 }
 
-interface IState {
+interface IDiscussionState {
     messagesState:  {id: string,
 		name: string,
 		body: string,
 		photo: string,
-		date: string}[];
+		date: string}[],
+	inputIsEmpty: boolean
 	
 }
 
-/* mock props
-**
-		const messages = 
-		[{
-			id: '1',
-			name: 'Morris',
-			body: 'Nice to see you',
-			photo: 'https://img.icons8.com/dusk/64/000000/circled-user-male-skin-type-6.png',
-			date: '08.12.2017 12:35'
-		}];
-
-*/
-
-class DiscussionComponent extends Component<IDiscussionProps, IState> {
-	// static defaultProps: IDiscussionProps = {
-    //     messages: []
-	// }
-	// state:IState = {
-    //     messagesState: this.props.messages
-    // }
+class DiscussionComponent extends Component<IDiscussionProps, IDiscussionState> {
 	constructor(props:IDiscussionProps){
 		super(props);
-		this.state = { messagesState: this.props.messages };
+		this.state = {
+			messagesState: this.props.messages,
+			inputIsEmpty: true
+		};
 	  }
 	private newMessage = createRef<HTMLTextAreaElement>();
-	private userName = createRef<HTMLDivElement>();
 	private userPhoto = createRef<HTMLImageElement>();
-	pad(n:any) {
+	inputChange = () => {
+		if (this.newMessage.current && this.newMessage.current.value.match(/^(?!\s*$).*/)){
+			this.setState({
+				inputIsEmpty: false
+			});
+		} else {
+			this.setState({
+				inputIsEmpty: true
+			});
+		}
+	}
+	pad(n:number) {
 		return n < 10 ? '0' + n : n;
 	};
 	addMessage = () => {
-		if (this.userName.current && this.newMessage.current && this.userPhoto.current){
-			const date = new Date;
+		if (this.newMessage.current && this.userPhoto.current){
+			const date = new Date();
 			let dateString = [date.getFullYear(),
 				this.pad(date.getMonth()+1),
 				this.pad(date.getDate())
@@ -61,7 +58,7 @@ class DiscussionComponent extends Component<IDiscussionProps, IState> {
 
 			let newMessageItem = {
 				id: (Math.random() * (9000 - 1) + 1).toString(),
-				name: this.userName.current.textContent ? this.userName.current.textContent : 's',
+				name: 'Me',
 				photo: this.userPhoto.current.src,
 				body: this.newMessage.current.value,
 				date: dateString
@@ -71,16 +68,16 @@ class DiscussionComponent extends Component<IDiscussionProps, IState> {
 			let arr = this.state.messagesState;
 			arr.push(newMessageItem);
 			this.setState({
-				messagesState: arr
+				messagesState: arr,
+				inputIsEmpty: true
 			});
 		}
-		
 	}
 	render(){
 		const  messages  = this.state.messagesState;
 		return (
-			<div className="UserDiscussionComponent">
-				<h1>Chat Discussion</h1>
+			<div className="UserDiscussionComponent" id="scroller">
+				<div className="MessageContainer">
 				{messages.map(message =>
 					<div className="messageItem" key={message.id}>
 						<img src={message.photo} alt="userPhoto"/>
@@ -93,19 +90,16 @@ class DiscussionComponent extends Component<IDiscussionProps, IState> {
 						</div>
 					</div>
 				)}
-				<div className="messageItem newMessageItem">
-						<img src="https://img.icons8.com/wired/64/000000/user-male-circle.png" alt="userPhoto" ref={this.userPhoto}/>
-						<div className="messageBody">
-							<div className="messageInfo">
-								<div className="name" ref={this.userName}>Me</div>
-								<div className="date">Now</div>
-							</div>
-							<div className="newMessage">
-								<textarea className="newMessageInput" wrap="off" ref={this.newMessage} placeholder="Type a message"></textarea>
-								<button onClick={this.addMessage}>SEND</button>
-							</div>
+				</div>
+				<div className="messageItem newMessageItem" id="anchor">
+					<img src="https://img.icons8.com/wired/64/000000/user-male-circle.png" alt="userPhoto" ref={this.userPhoto}/>
+					<div className="messageBody">
+						<div className="newMessage">
+							<textarea className="newMessageInput" wrap="off" ref={this.newMessage} placeholder="Type a message" onChange={this.inputChange}></textarea>
+							<button onClick={this.addMessage} disabled={this.state.inputIsEmpty}>SEND</button>
 						</div>
 					</div>
+				</div>
 			</div>
 		);
 	}
