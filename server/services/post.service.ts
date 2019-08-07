2349827fcb7b3a1af8c5f334e79b292552ed3e15
igor185 <igor.babin185@gmail.com>
2019-08-07 23:37:37 +0300

    fix-relations-in-db: fix get posts

--- server/services/post.service.ts
+++ server/services/post.service.ts
@@ -1,13 +1,12 @@
-import {Post} from '../models/PostModel';
+import {Post} from '../models/PostModel'
+
 import PostRepository from '../repository/post.repository';
-import {getCustomRepository, getRepository} from "typeorm";
+import {getCustomRepository} from "typeorm";
 
 export const createPost = async (post: Post): Promise<Post> =>
     await getCustomRepository(PostRepository)
         .save(post);
 
 export const getPosts = async (): Promise<Post[]> =>
-    await getRepository(Post)
-        .createQueryBuilder("Post")
-        .leftJoinAndSelect("Post.userId", "id")
-        .getMany();
+    await getCustomRepository(PostRepository)
+        .find();
