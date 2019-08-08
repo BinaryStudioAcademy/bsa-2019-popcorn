24e944ee9b16b3e02572c20d07e1a0ff922e0c34
Vera Shreder <v.shreder@lenal.biz>
2019-08-08 17:46:23 +0300

    mock-movie:add mock service

---
+++ server/services/mock.service.ts
@@ -0,0 +1,43 @@
+import { Movie } from '../models/MovieModel'
+import fetch from 'node-fetch';
+
+function api(url: string): Promise < any > {
+	return fetch(url)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(response.statusText)
+			}
+			return response.json()
+		})
+}
+
+async function asyncForEach(array, callback) {
+	for (let index = 0; index < array.length; index++) {
+		await callback(array[index], index, array);
+	}
+}
+
+export const getMovieList = async (pageNum: number = 1): Promise < Movie[] > => {
+	return await api(`https://api.themoviedb.org/3/movie/popular?api_key=11d5b4ce87f77b5efa7ddaef0cadb6b3&language=en-US&page=${pageNum}`)
+		.then(async (movies) => {
+			let newMovies = [];
+			const startForeach = async () => {
+				await asyncForEach(movies.results, async (element) => {
+					let movieDetails = await api(`https://api.themoviedb.org/3/movie/${element.id}?api_key=11d5b4ce87f77b5efa7ddaef0cadb6b3&language=en-US`);
+					let newMovie = new Movie;
+					newMovie.title = element.title;
+					newMovie.year = Number(element.release_date.split('-')[0]);
+					newMovie.duration = movieDetails.runtime ? movieDetails.runtime : 0;
+					newMovie.preview = `https://image.tmdb.org/t/p/w600_and_h900_bestv2/${element.backdrop_path}`;
+					newMovie.description = element.overview;
+					newMovies.push(newMovie);
+				});
+				return (newMovies);
+			}
+			return startForeach();
+		})
+		.catch(error => {
+			console.log(error.message);
+			return error;
+		})
+}
