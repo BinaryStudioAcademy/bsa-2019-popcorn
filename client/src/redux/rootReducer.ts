3e3018b1fdcfc822f2a16bcea8a4a4bdcba86f70
Kirill Burmelov <burik0304@icloud.com>
2019-08-08 15:39:59 +0300

    clean-up-repo: fix redux folder after delete TestComponent

--- client/src/redux/rootReducer.ts
+++ client/src/redux/rootReducer.ts
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
-import testReducer from "./../components/TestComponent/TestComponent.reducer";
+
 
 const reducers = {
-    test: testReducer,
+    
 };
 
 export default combineReducers({
-    ...reducers
+    
 })
