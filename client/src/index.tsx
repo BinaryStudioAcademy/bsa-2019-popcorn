ecb2014ef4177cd8d37fd88e53383dea2ab1619e
Alyona Rapova <alyona.rapova@binary-studio.com>
2019-08-08 19:11:07 +0300

    Remove extra redux provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import "./styles/index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <App />,
     document.getElementById("root")
 );
 
