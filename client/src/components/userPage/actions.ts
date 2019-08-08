6d754bbb2959973b573864f94660bf14b12e70d7
igor185 <igor.babin185@gmail.com>
2019-08-08 23:17:06 +0300

    implement-upload-avatar: save avatar to user and set new avatar to state

--- client/src/components/userPage/actions.ts
+++ client/src/components/userPage/actions.ts
@@ -1,11 +1,11 @@
 import {START_UPLOAD_AVATAR} from "./actionTypes";
 
 
-export const uploadAvatar = (file: FormData) =>{
+export const uploadAvatar = (file: FormData, id: string): any => {
     return {
         type: START_UPLOAD_AVATAR,
-        payload:{
-            file
+        payload: {
+            file, id
         }
     }
 };
--- client/src/components/userPage/saga.ts
+++ client/src/components/userPage/saga.ts
@@ -4,12 +4,18 @@ import {uploadFile} from "../../services/file.service";
 import axios from 'axios';
 
 export function* uploadAvatar(action) {
-
     try {
         const data = yield call(uploadFile, action.payload.file);
-        console.log(data);
-        yield call(axios.post, {image: data.imageUrl});
-        yield put({type: FINISH_UPLOAD_AVATAR,payload:{image:data.imageUrl}});
+
+        // remove public in order to save public path to img in server
+        let url =  (data.imageUrl).split(`\\`);
+        url.shift();
+
+
+        
+        const res = yield call(axios.put, `http://localhost:5000/api/user/${action.payload.id}`, {avatar: "http://localhost:5000/"+url.join('/')});
+
+        yield put({type: FINISH_UPLOAD_AVATAR,payload:{user: res.data.data.user}});
     }catch (e) {
         console.log("user page saga catch", e.message);
     }
@@ -18,4 +24,10 @@ export function* uploadAvatar(action) {
 
 function* watchFetchFilms(){
     yield takeEvery(START_UPLOAD_AVATAR, uploadAvatar);
+}
+
+export default function* header() {
+    yield all([
+        watchFetchFilms()
+        ])
 }
