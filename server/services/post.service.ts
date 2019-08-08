62863418cfcd531020116c1c52427540601d6b25
Kirill Burmelov <burik0304@icloud.com>
2019-08-08 09:54:40 +0300

    post-api: fix function createPost

--- server/services/post.service.ts
+++ server/services/post.service.ts
@@ -1,10 +1,14 @@
 import { Post } from '../models/PostModel';
 import PostRepository from '../repository/post.repository';
+import UserRepository from '../repository/user.repository'
 import { getRepository, getCustomRepository } from "typeorm";
 
-export const createPost = async (post: Post): Promise<Post> => 
-    await getCustomRepository(PostRepository)
+export const createPost = async (post: any): Promise<Post> => {
+
+    post.user = await getCustomRepository(UserRepository).findOne(post.userId);
+    return await getCustomRepository(PostRepository)
         .save(post);
+}
 
     
 export const getPosts = async (): Promise<Post[]> =>
