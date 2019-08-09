6572c11f811cb9cfe47da7da44d0ce38776460d7
Alexey Tryapko <aatryapko1201@gmail.com>
2019-08-09 21:25:32 +0300

    deploy: comment error seed

--- server/seed/index.ts
+++ server/seed/index.ts
@@ -3,6 +3,7 @@ import {SeedPosts1565202279037} from './1565202279037-SeedPosts';
 import { SeedMovies1565181039423 } from './1565181039423-SeedMovies';
 
 
-export default [SeedUsers1565158741121, SeedPosts1565202279037, SeedMovies1565181039423];
+// export default [SeedUsers1565158741121, SeedPosts1565202279037, SeedMovies1565181039423];
+export default [SeedUsers1565158741121, SeedPosts1565202279037];
 //to create new migration run in this folder: typeorm migration:create -n <name>
 // then import this migration hear and export in array below
