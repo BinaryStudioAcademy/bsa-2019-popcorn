8fb2f4d539f58dcee31a602385b13b7b80b781fc
Alexey Tryapko <aatryapko1201@gmail.com>
2019-08-09 13:17:29 +0300

    erro-page-error-handling: add module

--- client/src/containers/Main/main.tsx
+++ client/src/containers/Main/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import NotFound from './../../components/NotFound/NotFound';
 import MainPageSidebar from "../../components/shared/MainSidebar/MainPageSidebar";
 import MovieList from '../../components/MovieList/MovieList';
 
