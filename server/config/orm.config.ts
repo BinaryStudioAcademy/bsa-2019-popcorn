67d4bfb1ba2c987bbcf0b06a8d1986f1b1b96aca
Alexey Tryapko <aatryapko1201@gmail.com>
2019-08-11 15:44:55 +0300

    deploy: change db conection_config

--- server/config/orm.config.ts
+++ server/config/orm.config.ts
@@ -2,26 +2,23 @@ import {ConnectionOptions} from "typeorm";
 import entities from '../entities/index';
 import migrations from '../seed/index';
 
-const db_config : ConnectionOptions = {
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  synchronize: true,
-  logging: false,
-  entities,
-  migrations
-};
+const {
+  DATABASE_URL,
+  DB_HOST,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+} = process.env;
 
-const url_db_config : ConnectionOptions =  {
+const url = DATABASE_URL  ||  `postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
+
+const db_config : ConnectionOptions =  {
     type: 'postgres',
-    url: `postgresql://${process.env.URL_DB_USERNAME}:${process.env.URL_DB_PASSWORD}@${process.env.URL_DB_HOST}/${process.env.URL_DB_NAME}`,
+    url,
     synchronize: true,
     logging: false,
     entities,
     migrations
 }
 
-export default [db_config, url_db_config];
+export default db_config;
