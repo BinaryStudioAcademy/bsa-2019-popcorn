b5973ac876a4cceded7ee61dc583f4b3c48eeb37
igor185 <igor.babin185@gmail.com>
2019-08-12 11:53:01 +0300

    connect-posts-events: render stories from store

--- client/src/components/MainPage/StoryList/story-list/story-list.tsx
+++ client/src/components/MainPage/StoryList/story-list/story-list.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import StoryListContent from '../story-list-content/story-list-content';
 import AddStoryItem from '../add-story-item/add-story-item';
 import StoryListSlider from "../story-list-slider/story-list-slider";
@@ -14,39 +14,42 @@ interface IStoryListItem {
     imageUrl: string,
     avatar: string,
 };
+
 interface IStoryListProps {
     scrollStep: number,
+    stories: null | Array<IStoryListItem>,
+    fetchStories: () => any
 }
 
-class StoryList extends Component<IStoryListProps>{
+class StoryList extends Component<IStoryListProps> {
     mockedimageUrl = 'https://s3-alpha-sig.figma.com/img/e0c3/dd6e/d2d5bd4e0e1d9243f70ca5d2ba5f9130?Expires=1566172800&Signature=PZn4tI7Bfttzi9p9SM7hNsacTq8ul-9qr0c1X4n7MAFCDK8DIpdqN~YpdtLJO3ozvXCF1pWAxaOHdGkvIyuDdL5dYIVtgEOMf7S1oD5woUmPcF0C2PQ0p1mkMY5-QPEbrMy4e5~uuPvZo8XPaZaRypwBqNU7JPhZb8~6ZUt-xZJ8zum-lYmR4RWhPLfb02g3bPNCYRYw8c-Wuqr6WNfOQEeueUXyNl0HQCg9BK2hY3Lyz5s7FizfNzRVL9QZSsPdDbP-tbEZK~PUNlEPRn7Aw0JcEYMuJ1NuMoNOFHTYVvpFcKe0akNq9NDqDcWv2UaCB6NnsbxDIBejoN5EeuHozA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA';
     mockedText = "example";
     mockedAvatar = "https://pbs.twimg.com/profile_images/1148972773290762240/kmkJzp5I_400x400.png";
     array = [
-        { imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "1", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "2", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "3", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "4", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "5", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "6", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "7", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "8", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "9", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "123", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "5", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "6", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "7", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "8", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "9", avatar: this.mockedAvatar },
-        { imageUrl: this.mockedimageUrl, name: "123", avatar: this.mockedAvatar },
+        {imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: this.mockedText, avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "1", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "2", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "3", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "4", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "5", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "6", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "7", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "8", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "9", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "123", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "5", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "6", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "7", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "8", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "9", avatar: this.mockedAvatar},
+        {imageUrl: this.mockedimageUrl, name: "123", avatar: this.mockedAvatar},
     ];
 
     state = {
@@ -54,16 +57,17 @@ class StoryList extends Component<IStoryListProps>{
         scrollStep: 0,
         isPopupShown: false,
     }
+
     componentDidMount() {
-        this.setState({ scrollStep: this.props.scrollStep });
+        this.setState({scrollStep: this.props.scrollStep});
     };
 
     onOpenPopupClick = () => {
-        this.setState({ isPopupShown: true });
+        this.setState({isPopupShown: true});
     }
 
     onClosePopupClick = () => {
-        this.setState({ isPopupShown: false });
+        this.setState({isPopupShown: false});
     }
 
     scrollLeft = () => {
@@ -88,24 +92,25 @@ class StoryList extends Component<IStoryListProps>{
         })
     }
 
-    getStoryRange = (storyListItems: Array<IStoryListItem>, index: number) => {
-        return storyListItems.slice(index, index + 9);
+    getStoryRange = (storyListItems: null | Array<IStoryListItem>, index: number) => {
+        return storyListItems ? storyListItems.slice(index, index + 9) : [];
     }
 
     render() {
         return (<div className="story-list-wrapper">
-            <AddStoryPopup onClosePopupClick={this.onClosePopupClick} isShown={this.state.isPopupShown} />
-            <StoryListSlider scrollLeft={this.scrollLeft} scrollRight={this.scrollRight} />
+            <AddStoryPopup onClosePopupClick={this.onClosePopupClick} isShown={this.state.isPopupShown}/>
+            <StoryListSlider scrollLeft={this.scrollLeft} scrollRight={this.scrollRight}/>
             <div className="story-list">
-                <AddStoryItem onOpenPopupClick={this.onOpenPopupClick} avatar={this.mockedAvatar} />
-                <StoryListContent storyListItems={this.getStoryRange(this.array, this.state.index)} />
+                <AddStoryItem onOpenPopupClick={this.onOpenPopupClick} avatar={this.mockedAvatar}/>
+                <StoryListContent storyListItems={this.getStoryRange(this.props.stories, this.state.index)}/>
             </div>
         </div>);
     }
 };
 
 const mapStateToProps = (rootState, props) => ({
-    ...props
+    ...props,
+    stories: rootState.story.stories
 });
 
 const actions = {
--- client/src/redux/rootReducer.ts
+++ client/src/redux/rootReducer.ts
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import movieReducer from '../components/MovieSeriesPage/Movie.redux/reducer';
 import profileHeader from '../components/UserPage/reducer';
+import storyReducer from '../components/MainPage/StoryList/story.redux/reducer';
 
 const reducers = {};
 
 export default combineReducers({
 	...reducers,
 	movie: movieReducer,
-	profile: profileHeader
+	profile: profileHeader,
+	story: storyReducer
 });
