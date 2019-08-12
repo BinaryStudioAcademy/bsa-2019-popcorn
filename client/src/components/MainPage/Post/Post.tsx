260dad50e68d885da91ffe2a706957a1e1301366
Kirill Burmelov <burik0304@icloud.com>
2019-08-12 11:45:16 +0300

    review-tab: delete command !important in review-tab styles and header

--- client/src/components/MainPage/Post/Post.tsx
+++ client/src/components/MainPage/Post/Post.tsx
@@ -1,67 +1,78 @@
-import React, { PureComponent } from "react";
-import AddComment from "../../shared/AddComment/AddComment"
-import "./Post.scss"
+import React, { PureComponent } from 'react';
+import AddComment from '../../shared/AddComment/AddComment';
+import './Post.scss';
 import { ReactComponent as SettingIcon } from '../../../assets/icons/general/settings.svg';
 import { ReactComponent as LikeIcon } from '../../../assets/icons/general/likeIcon.svg';
 import { ReactComponent as CommentIcon } from '../../../assets/icons/general/commentIcon.svg';
 import { ReactComponent as ShareIcon } from '../../../assets/icons/general/shareIcon.svg';
-import Comment from "../Comment/Comment";
-import Tag from "../Tag/Tag";
-import PostEditModal from "../PostEditModal/PostEditModal";
-import PostContent from "../PostContent/PostContent";
+// import Comment from "";
+// import Tag from "../Tag/Tag";
+import PostEditModal from '../PostEditModal/PostEditModal';
+import PostContent from '../PostContent/PostContent';
 
 type IPostProps = {
-    post:{
-		author: string,
-		authorImage: string,
-		postDate: string,
-		postImage: string,
-		body?: string,
+	post: {
+		author: string;
+		authorImage: string;
+		postDate: string;
+		postImage: string;
+		body?: string;
 		content?: {
-			image: string,
-			link: string,
-			description: string
-		},
+			image: string;
+			link: string;
+			description: string;
+		};
 		comments?: {
-			id: string,
-			author: string,
-			commentDate: string,
-			commentBody: string
-		}[],
+			id: string;
+			author: string;
+			commentDate: string;
+			commentBody: string;
+		}[];
 		tags?: {
-			id: string,
-			tagName: string
-		}[]
-	}
-}
-
-interface IPostState {
-    isModalShown: boolean,
+			id: string;
+			tagName: string;
+		}[];
+	};
 };
 
+interface IPostState {
+	isModalShown: boolean;
+}
+
 class Post extends PureComponent<IPostProps, IPostState> {
-    constructor(props: IPostProps) {
-        super(props);
-        this.state = { 
-            isModalShown: false
+	constructor(props: IPostProps) {
+		super(props);
+		this.state = {
+			isModalShown: false
 		};
 	}
 	isOwnPost() {
-        return true;
-    }
-	toggleModal = () => {
-        this.setState({ isModalShown: !this.state.isModalShown });
+		return true;
 	}
+	toggleModal = () => {
+		this.setState({ isModalShown: !this.state.isModalShown });
+	};
 	isModalShown() {
-        return this.state.isModalShown ? 
-            <PostEditModal isOwn={this.isOwnPost()} /> :
-            null;
-    }
-	render () {
-		const {post: {author, authorImage, postDate, postImage, body, content, comments, tags}} =  this.props;
+		return this.state.isModalShown ? (
+			<PostEditModal isOwn={this.isOwnPost()} />
+		) : null;
+	}
+	render() {
+		const {
+			post: {
+				author,
+				authorImage,
+				postDate,
+				postImage,
+				body,
+				content,
+				comments,
+				tags
+			}
+		} = this.props;
 		return (
-			<div className='post-item'>
-				<div className='post-item-header'>
+			<div className="post-item">
+				{/* <div className='post-item-header'>
 					<img className='post-item-avatar' src={authorImage} alt="author" />
 					<div className='post-item-info'>
 						<div className='post-item-author-name'>{author}</div>
@@ -112,10 +123,10 @@ class Post extends PureComponent<IPostProps, IPostState> {
 				</div>
 				}
 				<div className="horizontal-stroke"></div>
-				<AddComment></AddComment>
+				<AddComment></AddComment> */}
 			</div>
 		);
-	}   
+	}
 }
 
 export default Post;
