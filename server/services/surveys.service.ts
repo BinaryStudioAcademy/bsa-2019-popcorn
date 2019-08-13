40811fa5dde3ab4fbf1b722e48acc7e92c4e4e16
unknown <fakeu.at@gmail.com>
2019-08-13 22:01:30 +0300

    change type of surveys field in UserModel

--- server/services/surveys.service.ts
+++ server/services/surveys.service.ts
@@ -1,43 +1,55 @@
-import {getCustomRepository} from "typeorm";
-import {Surveys} from '../models/SurveysModel';
-import {SurveysQuestion} from '../models/SurveysQuestionModel';
-import SurveysRepository from '../repository/surveys.repository';
+import { getCustomRepository } from "typeorm";
+import { Surveys } from "../models/SurveysModel";
+import { SurveysQuestion } from "../models/SurveysQuestionModel";
+import SurveysRepository from "../repository/surveys.repository";
 
 interface IRequest {
-  userId: string,
-  surveys: Surveys, 
-  surveysQuestion: SurveysQuestion[]
+  userId: string;
+  surveys: Surveys;
+  surveysQuestion: SurveysQuestion[];
 }
 
 export const createSurveys = async (body: IRequest, next): Promise<Surveys> => {
-  return await getCustomRepository(SurveysRepository).createSurveys(body.userId, body.surveys, body.surveysQuestion, next);
+  return await getCustomRepository(SurveysRepository).createSurveys(
+    body.userId,
+    body.surveys,
+    body.surveysQuestion,
+    next
+  );
 };
 
 export const getSurveys = async (next): Promise<Surveys[]> => {
-    return await getCustomRepository(SurveysRepository).getSurveys(next);
+  return await getCustomRepository(SurveysRepository).getSurveys(next);
 };
 
 export const getSurveysById = async (id: string, next): Promise<Surveys> => {
   return await getCustomRepository(SurveysRepository).getSurveysById(id, next);
-}
+};
 
-export const deleteSurveysById = async (id: string, next): Promise<{ }> => {
-  return await getCustomRepository(SurveysRepository).deleteSurveysById(id, next);
-}
+export const deleteSurveysById = async (id: string, next): Promise<{}> => {
+  return await getCustomRepository(SurveysRepository).deleteSurveysById(
+    id,
+    next
+  );
+};
 
-export const updateSurveysById = async (id: string, surveys: Surveys, next): Promise<Surveys> => {
-  return await getCustomRepository(SurveysRepository).updateSurveysById(id, surveys, next);
-}
+export const updateSurveysById = async (id: string, next): Promise<Surveys> => {
+  return await getCustomRepository(SurveysRepository).updateSurveysById(
+    id,
+    next
+  );
+};
 
-export const getSurveysByUserId = async (id: string, next): Promise<Surveys[]> => {
-  return await getCustomRepository(SurveysRepository).getSurveysByUserId(id, next);
-}
-
-
-
-
+export const getSurveysByUserId = async (
+  id: string,
+  next
+): Promise<Surveys[]> => {
+  return await getCustomRepository(SurveysRepository).getSurveysByUserId(
+    id,
+    next
+  );
+};
 
 // export const createSurveysQuestionBySurveysId = async (id: string, surveysOption: SurveysQuestion, next): Promise<SurveysQuestion> => {
 //   return await getCustomRepository(SurveysRepository).createSurveysQuestionBySurveysId(id, surveysOption, next);
 // }
-
