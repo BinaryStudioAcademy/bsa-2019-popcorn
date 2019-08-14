3e0b17e6dab751e77508c7bddf6de14242d60293
Alexey Tryapko <aatryapko1201@gmail.com>
2019-08-14 23:44:35 +0300

    merge dev

--- server/seed/index.ts
+++ server/seed/index.ts
@@ -7,8 +7,8 @@ import { SeedStory1565606634607 } from "./1565606634607-SeedStory";
 export default [
   SeedUsers1565158741121,
   SeedPosts1565202279038,
-  SeedStory1565606634607,
-  SeedMovies1565181039423
+  SeedStory1565606634607
+  // SeedMovies1565181039423
 ];
 //to create new migration run in this folder: typeorm migration:create -n <name>
 // then import this migration hear and export in array below
