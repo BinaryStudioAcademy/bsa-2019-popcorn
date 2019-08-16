3ea3cf17a426fe5bb4307f29d43cda76f11afee8
Kirill Burmelov <burik0304@icloud.com>
2019-08-16 13:02:44 +0300

    fix: fix path to default avatar for user

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const config = {
 	API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
-	DEFAULT_EVENT_IMAGE: './images/default/default-event.png',
-	DEFAULT_AVATAR: './images/default/avatar-default.png'
-	// 'https://forwardsummit.ca/wp-content/uploads/2019/01/avatar-default.png'
+	DEFAULT_AVATAR: '/images/default/avatar-default.png'
 };
 export default config;
