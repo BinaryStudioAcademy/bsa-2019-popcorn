557721f87f7d49b685bd154a8282a2afbbee27a5
Kirill Burmelov <burik0304@icloud.com>
2019-08-16 20:17:25 +0300

    add default image for movie

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,6 +2,7 @@ require('dotenv').config();
 const config = {
 	API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
 	DEFAULT_AVATAR: '/images/default/avatar-default.png',
-	DEFAULT_EVENT_IMAGE: '/images/default/event-default.png'
+	DEFAULT_EVENT_IMAGE: '/images/default/event-default.png',
+	DEFAULT_MOVIE_IMAGE: '/images/default/movie-default.png'
 };
 export default config;
