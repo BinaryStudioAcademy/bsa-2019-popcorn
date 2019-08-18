9e5248734ff86df61a1e59886a83f8a023528305
Taras <tarasnazar93@gmail.com>
2019-08-18 19:16:56 +0300

    feature/user-posts: small fix style

--- client/src/components/UserPage/UserPosts/UserPosts.tsx
+++ client/src/components/UserPage/UserPosts/UserPosts.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import PostList from '../../MainPage/PostList/PostList';
 import Spinner from '../../shared/Spinner';
 interface IProps {
-	posts?: any; // todo
+	posts?: any;
 	getUsersPosts: () => any;
 }
-
+const userPostStyle = {
+	marginRight: '0px',
+	paddingRight: '0px',
+	marginTop: '40px'
+};
 const UserPosts: React.FC<IProps> = ({ posts, getUsersPosts }) => {
 	if (!posts) {
 		getUsersPosts();
@@ -13,7 +17,11 @@ const UserPosts: React.FC<IProps> = ({ posts, getUsersPosts }) => {
 	}
 	return (
 		<div className="UserPosts">
-			<PostList posts={posts} />
+			{posts ? (
+				<PostList styleCustom={userPostStyle} type="userPosts" posts={posts} />
+			) : (
+				<div className="no-info-yet">No posts yet</div>
+			)}
 		</div>
 	);
 };
