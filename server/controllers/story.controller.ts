2541572a212b70663ef01681ef11fa000c76e3db
igor185 <igor.babin185@gmail.com>
2019-08-18 14:53:24 +0300

    set-up-socket: emit action after create story

--- server/controllers/story.controller.ts
+++ server/controllers/story.controller.ts
@@ -15,9 +15,12 @@ router
       .then((response: Story) => res.send(response))
       .catch(next)
   )
-  .post("/", (req: Request, res: Response, next: NextFunction) =>
+  .post("/", (req: Request & { io: any }, res: Response, next: NextFunction) =>
     StoryService.createStory(req.body)
-      .then((response: Story) => res.send(response))
+      .then((response: Story) => {
+        req.io.emit("new-story", response);
+        res.send(response);
+      })
       .catch(next)
   )
   .put("/", (req: Request, res: Response, next: NextFunction) =>
