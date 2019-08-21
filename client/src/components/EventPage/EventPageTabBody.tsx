6f197d2694b75eef2678bdf705498adf8b58edd8
alex_yavor <alex.yavor1@gmail.com>
2019-08-21 23:18:25 +0300

    general-events: implement interested tab

--- client/src/components/EventPage/EventPageTabBody.tsx
+++ client/src/components/EventPage/EventPageTabBody.tsx
@@ -4,13 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import About from './About/About';
 import Discussion from './Discussion/Discussion';
 import ParticipantList from './ParticipantList/ParticipantList';
-import InterestedList from './InterestedList/InterestedList';
 import { IEventFormatClient } from '../UserPage/UserEvents/UserEvents.service';
 
 interface IProps {
 	mainPath: string;
 	event: IEventFormatClient;
 }
+const INTERESTED_STATUS = 'interested';
+const GOING_STATUS = 'going';
 
 const EventPageTabBody: React.SFC<IProps> = ({ mainPath, event }) => {
 	return (
@@ -24,11 +25,21 @@ const EventPageTabBody: React.SFC<IProps> = ({ mainPath, event }) => {
 				<Route path={`${mainPath}/discussion`} render={() => <Discussion />} />
 				<Route
 					path={`${mainPath}/participants`}
-					render={() => <ParticipantList participants={event.eventVisitors} />}
+					render={() => (
+						<ParticipantList
+							participants={event.eventVisitors}
+							status={GOING_STATUS}
+						/>
+					)}
 				/>
 				<Route
 					path={`${mainPath}/interested`}
-					render={() => <InterestedList />}
+					render={() => (
+						<ParticipantList
+							participants={event.eventVisitors}
+							status={INTERESTED_STATUS}
+						/>
+					)}
 				/>
 			</Switch>
 		</div>
--- client/src/components/EventPage/ParticipantList/ParticipantList.tsx
+++ client/src/components/EventPage/ParticipantList/ParticipantList.tsx
@@ -2,19 +2,26 @@ import React from 'react';
 import { IVisitor } from '../../UserPage/UserEvents/UserEvents.service';
 interface IProps {
 	participants: IVisitor[];
+	status: string;
 }
 
-const ParticipantList: React.FC<IProps> = ({ participants }) => {
-	// console.log("users", users);
+const ParticipantList: React.FC<IProps> = ({ participants, status }) => {
+	const sortedParticipantsByStatus = participants.filter(
+		participant => participant.status === status
+	);
+
+	if (sortedParticipantsByStatus.length === 0) {
+		return <div>No one is {status}.</div>;
+	}
+
 	return (
 		<div className="participant-list">
-			{participants.map(participant => (
+			{sortedParticipantsByStatus.map(participant => (
 				<div>
 					<div className="participant-avatar">
 						<img src={participant.user.avatar} alt="" />
 					</div>
 					<div className="participant-name">{participant.user.name}</div>
-					<div className="participant-status">{participant.status}</div>
 				</div>
 			))}
 		</div>
