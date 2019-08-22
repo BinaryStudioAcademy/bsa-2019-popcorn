229d8202601e470534597d55d4a337bfc2b4af71
Roman Miniv <1pi15bminivroman@gmail.com>
2019-08-22 12:52:45 +0300

    feature/top-film-page: added initial html structure

--- client/src/components/TopPage/TopPage.tsx
+++ client/src/components/TopPage/TopPage.tsx
@@ -4,7 +4,7 @@ interface ITopPageProps {
 	match: {
 		path: string;
 		params: any;
-	};
+    };
 }
 
 const TopPage: React.SFC<ITopPageProps> = ({
@@ -13,6 +13,34 @@ const TopPage: React.SFC<ITopPageProps> = ({
     return (
         <div className="top-page">
             {`top-page/${match.params.id}`}
+            <div className="top">
+                <img src="" alt="top-image" />
+                <div className="top-description">
+                    <span className="top-title"></span>
+                    <div className="top-author">
+                        <img src="" alt="author-image" />
+                        <span className="top-author-name"></span>
+                    </div>
+                    <span className="top-created-at"></span>
+                </div>
+            </div>
+            <div className="top-movie-list">
+                <ol>
+                    <li className="top-movie-list-item">
+                        <img src="" alt="movie-image" />
+                        <div className="movie-description">
+                            <div>
+                                <span className="movie-title"></span>
+                                <span className="movie-year"></span>
+                            </div>
+                            <div className="movie-genres">
+
+                            </div>
+                            <span className="author-comment"></span>
+                        </div>
+                    </li>
+                </ol>
+            </div>
         </div>
     )
 }
