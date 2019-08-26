9387d424f472bb314e5d921666ab8a54676713d9
Kirill Burmelov <burik0304@icloud.com>
2019-08-26 20:31:31 +0300

    watch-user: init watchList saga

---
+++ client/src/components/UserPage/UserWatchList/saga.ts
@@ -0,0 +1,13 @@
+import { all, call, put, takeEvery } from 'redux-saga/effects';
+
+// export function* (action) {
+
+// }
+
+// function* watch...() {
+// 	yield takeEvery(action type, saga function);
+// }
+
+export default function* review() {
+	yield all([]);
+}
--- client/src/redux/saga.ts
+++ client/src/redux/saga.ts
@@ -10,6 +10,7 @@ import surveySaga from '../components/UserPage/UserSurveys/UserSurveys.redux/sag
 import reviewSaga from '../components/MovieSeriesPage/MovieSeriesReviews/saga';
 import topListSaga from '../components/TopListPage/TopListPage.redux/saga';
 import followSaga from '../components/UserPage/ProfileComponent/FollowSystem/FollowSystem.redux/saga';
+import watchListSaga from '../components/UserPage/UserWatchList/saga';
 
 export default function* rootSaga() {
 	yield all([
@@ -23,6 +24,7 @@ export default function* rootSaga() {
 		surveySaga(),
 		reviewSaga(),
 		topListSaga(),
-		followSaga()
+		followSaga(),
+		watchListSaga()
 	]);
 }
