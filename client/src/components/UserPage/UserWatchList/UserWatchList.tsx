d62a107834c39e930b0f90ee43b33fa508a62dd6
Kirill Burmelov <burik0304@icloud.com>
2019-08-26 21:43:58 +0300

    watch-user: add fetchWatchList

--- client/src/components/UserPage/UserWatchList/UserWatchList.tsx
+++ client/src/components/UserPage/UserWatchList/UserWatchList.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWatchList } from './actions';
+import TMovie from '../../MovieSeriesPage/TMovie';
+import Spinner from '../../shared/Spinner';
 
-const UserWatchList: React.FC = () => {
+interface IProps {
+	watchList: Array<IWatchItem>;
+	fetchWatchList: () => object;
+}
+
+interface IWatchItem {
+	id: string;
+	created_at: Date;
+	movie: TMovie;
+	status: string;
+}
+
+const UserWatchList: React.FC<IProps> = props => {
+	const { watchList, fetchWatchList } = props;
+
+	if (!watchList) {
+		fetchWatchList();
+		return <Spinner />;
+	}
+
 	return <div className="UserWatchList">UserWatchList</div>;
 };
 
-export default UserWatchList;
+const mapStateToProps = (state, props) => ({
+	...props,
+	watchList: state.watchList.watchList
+});
+
+const mapDispatchToProps = dispatch => {
+	const actions = {
+		fetchWatchList
+	};
+
+	return bindActionCreators(actions, dispatch);
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(UserWatchList);
--- client/src/components/UserPage/UserWatchList/reducer.ts
+++ client/src/components/UserPage/UserWatchList/reducer.ts
@@ -1,13 +1,18 @@
+import { FETCH_USER_WATCH_LIST_SUCCESS } from './actionTypes';
+
 interface IReducer {
-	watchList: Array<any>;
+	watchList?: Array<any>;
 }
 
 const initialState: IReducer = {
-	watchList: []
+	watchList: undefined
 };
 
 export default (state = initialState, action) => {
 	switch (action.type) {
+		case FETCH_USER_WATCH_LIST_SUCCESS:
+			return { ...state, watchList: action.payload.watchList };
+
 		default:
 			return state;
 	}
