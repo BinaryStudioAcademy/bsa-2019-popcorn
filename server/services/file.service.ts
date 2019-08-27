b4043c0f2bc5fae16a01add5059af88353b46c88
igor185 <igor.babin185@gmail.com>
2019-08-27 19:32:01 +0300

    unable upload inages from mobile

--- server/services/file.service.ts
+++ server/services/file.service.ts
@@ -1,22 +1,41 @@
-import {Form} from "multiparty";
+import { Form } from "multiparty";
+const uuid = require("uuid/v4");
+const fs = require("fs");
 
 export const uploadFile = req =>
-    new Promise<string>((resolve, reject) => {
-        const contentType = req.headers['content-type'];
+  new Promise<string>((resolve, reject) => {
+    const contentType = req.headers["content-type"];
 
+    if (contentType && contentType.indexOf("multipart") === 0) {
+      const form = new Form({
+        autoFiles: true,
+        uploadDir: "public/files",
+        maxFilesSize: 1048576 * 3
+      });
+      form.parse(req, function(err, fields, files): any {
+        console.log(files);
+        if (err) return reject(err);
 
-        if (contentType && contentType.indexOf('multipart') === 0) {
-            const form = new Form({
-                autoFiles: true,
-                uploadDir: "public/files",
-                maxFilesSize: 1048576*3
-            });
-            form.parse(req, function (err, fields, files): any {
-                if (!err)
-                    return resolve(files.file[0].path);
-                reject(err);
+        if (files.file) return resolve(files.file[0].path);
 
-            });
-        } else
-            reject(new Error("Bad request"))
-    });
+        if (files.image) return resolve(files.image[0].path);
+      });
+    } else if (contentType && contentType === "octet-stream") {
+      const buffer = [];
+      req.on("data", function onRequestData(chunk) {
+        buffer.push(chunk);
+      });
+
+      req.once("end", function() {
+        const concat = Buffer.concat(buffer);
+        req.body = JSON.parse(concat.toString("utf8"));
+
+        const format = req.body.format.split("/").pop();
+        const name = `public/files/${uuid()}.${format}`;
+        fs.writeFile(name, req.body.base, "base64", err => {
+          if (err) return reject(err);
+          return resolve(name);
+        });
+      });
+    } else reject(new Error("Bad request"));
+  });
