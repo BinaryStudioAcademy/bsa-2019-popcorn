d4cbc1693e704487ecbd73a721daf02c47972f39
Kirill Burmelov <burik0304@icloud.com>
2019-08-29 10:00:57 +0300

    watch-list: redesign watch list

--- client/src/components/UserPage/UserWatchList/WatchItem/WatchItem.tsx
+++ client/src/components/UserPage/UserWatchList/WatchItem/WatchItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IWatchItem } from '../UserWatchList';
 import './WatchItem.scss';
 import Image from '../../../shared/Image/Image';
@@ -7,7 +7,7 @@ import { NavLink } from 'react-router-dom';
 import { ReactComponent as DurationIcon } from '../../../../assets/icons/general/movie/duration-icon.svg';
 import getFilmDuration from '../../../../helpers/getFilmDuration';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faStar } from '@fortawesome/free-solid-svg-icons';
 import { ReactComponent as CloseIcon } from '../../../../assets/icons/general/closeIcon.svg';
 
 interface IProps {
@@ -28,11 +28,22 @@ const WatchItem: React.FC<IProps> = props => {
 	} = props.watchItem.movie;
 	const { moveToWatched, deleteWatchItem } = props;
 	const duration = getFilmDuration(runtime);
+	const [hover, setHover] = useState(false);
+
+	const hoverImage = () => {
+		setHover(!hover);
+	};
+
+	const vote_average = 3;
 
 	return (
 		<div className="WatchItem">
 			<div className="watch-item-container">
-				<div className="watch-image-wrapper">
+				<div
+					className="watch-image-wrapper"
+					onMouseEnter={hoverImage}
+					onMouseLeave={hoverImage}
+				>
 					<NavLink to={'/movies/' + movieId}>
 						<Image
 							src={poster_path}
@@ -40,53 +51,23 @@ const WatchItem: React.FC<IProps> = props => {
 							alt="poster"
 						/>
 					</NavLink>
+					{hover && <div className="absolute-data-in-image"></div>}
 				</div>
 				<div className="watch-main">
-					<div className="watch-main-top">
-						<div className="watch-movie-title">
-							<NavLink
-								className="watch-movie-title-nav-link"
-								to={'/movies/' + movieId}
-							>
-								<div className="movie-title-name">
-									{title}
-									<span className="movie-title-date">
-										{release_date
-											? ' (' + release_date.slice(0, 4) + ')'
-											: null}
-									</span>
+					<div className="watch-main-bottom">
+						<div className="watch-movie-title">{title}</div>
+						<div className="watch-secondary">
+							{duration && (
+								<div className="movie-duration">
+									<DurationIcon />
+									{duration}
 								</div>
-							</NavLink>
-						</div>
-						<div className="movie-genre">{genres}</div>
-						{duration && (
-							<div className="movie-duration">
-								<DurationIcon />
-								{duration}
+							)}
+							<div className="totaly-movie-rating">
+								<FontAwesomeIcon className="icon-star" icon={faStar} />
+								{Number(vote_average) || 0}
 							</div>
-						)}
-					</div>
-					<div className="watch-buttons">
-						{status === 'to_watch' && (
-							<button
-								className="move-button"
-								onClick={e => {
-									e.preventDefault();
-									moveToWatched(id);
-								}}
-							>
-								Move to Watched
-							</button>
-						)}
-						<button
-							className="delete-button"
-							onClick={e => {
-								e.preventDefault();
-								deleteWatchItem(id);
-							}}
-						>
-							<CloseIcon className="delete-button-svg" />
-						</button>
+						</div>
 					</div>
 				</div>
 			</div>
