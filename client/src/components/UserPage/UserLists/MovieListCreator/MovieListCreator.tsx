fe6f5033f0d4cf16bde47ae1b983ed93e3f7dedf
Kirill Burmelov <burik0304@icloud.com>
2019-08-31 19:48:23 +0300

    movie-list: add cropper to component

---
+++ client/src/components/UserPage/UserLists/MovieListCreator/MovieListCreator.tsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import './MovieListCreator.scss';
+import { INewMovieList } from '../UserLists';
+import { uploadFile } from '../../../../services/file.service';
+import Cropper from 'react-cropper';
+import ImageUploader from '../../../MainPage/ImageUploader/ImageUploader';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faTimesCircle,
+	faCheckCircle
+} from '@fortawesome/free-solid-svg-icons';
+
+interface IProps {
+	saveMovieList: (movieList: INewMovieList) => void;
+}
+
+interface IState {
+	image_url: string;
+	croppedSaved: boolean;
+}
+
+class MovieListCreator extends React.Component<IProps, IState> {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			image_url: '',
+			croppedSaved: false
+		};
+	}
+
+	private cropper = React.createRef<Cropper>();
+
+	imageStateHandler = (data, croppedSaved?: boolean) => {
+		this.setState({
+			image_url: data,
+			croppedSaved: croppedSaved ? croppedSaved : this.state.croppedSaved
+		});
+	};
+
+	onSaveCropped = () => {
+		if (this.cropper.current) {
+			const dataUrl = this.cropper.current.getCroppedCanvas().toBlob(blob => {
+				const data = new FormData();
+				data.append('file', blob);
+				uploadFile(data)
+					.then(({ imageUrl }) => {
+						if (imageUrl.indexOf('\\') !== -1) {
+							let url = imageUrl.split(`\\`);
+							url.shift();
+							url = url.join('/');
+
+							url = '/' + url;
+
+							this.imageStateHandler(url, true);
+						} else {
+							let url = imageUrl.split(`/`);
+							url.shift();
+							url = url.join('/');
+
+							url = '/' + url;
+
+							this.imageStateHandler(url, true);
+						}
+					})
+					.catch(error => {});
+			});
+		}
+	};
+
+	onCancel = () => {
+		this.setState({
+			image_url: '',
+			croppedSaved: false
+		});
+	};
+
+	render() {
+		return (
+			<div className="MovieListCreator">
+				<button className="button-image">
+					<ImageUploader
+						isIcon={true}
+						imageHandler={uploadFile}
+						imageStateHandler={this.imageStateHandler}
+					/>
+				</button>
+
+				{this.state.image_url && !this.state.croppedSaved && (
+					<div>
+						<Cropper
+							ref={this.cropper}
+							className="movie-list-creator-img"
+							src={this.state.image_url}
+							aspectRatio={16 / 9}
+						/>
+						<span onClick={this.onSaveCropped}>
+							<FontAwesomeIcon
+								icon={faCheckCircle}
+								className="fontAwesomeIcon"
+							/>
+						</span>
+						<span onClick={this.onCancel}>
+							<FontAwesomeIcon
+								icon={faTimesCircle}
+								className={'fontAwesomeIcon'}
+							/>
+						</span>
+					</div>
+				)}
+
+				{this.state.image_url && this.state.croppedSaved && (
+					<div className="movie-list-img-wrapper" style={{ width: '500px' }}>
+						<img
+							className="movie-list-img"
+							src={this.state.image_url}
+							style={{ width: '100%' }}
+						/>
+					</div>
+				)}
+			</div>
+		);
+	}
+}
+
+export default MovieListCreator;
--- client/src/components/UserPage/UserLists/UserLists.tsx
+++ client/src/components/UserPage/UserLists/UserLists.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import './UserLists.scss';
+import MovieListCreator from './MovieListCreator/MovieListCreator';
+
+export interface INewMovieList {
+	title: string;
+	isPrivate: boolean;
+	despription?: string;
+	image_url?: string;
+	moviesId: string[];
+}
 
 const UserLists: React.FC = () => {
-	return <div className="UserLists">UserLists</div>;
+	const saveMovieList = (movieList: INewMovieList) => {
+		console.log(movieList);
+	};
+
+	return (
+		<div className="UserLists">
+			<MovieListCreator saveMovieList={movieList => saveMovieList(movieList)} />
+		</div>
+	);
 };
 
 export default UserLists;
