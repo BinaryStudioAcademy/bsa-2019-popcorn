2e2191718a6b8cf14ba83f86094699ea9bf2a595
Taras <tarasnazar93@gmail.com>
2019-09-01 22:38:17 +0300

    feature/image-upload-s3: connect s3 as image storage

--- server/services/file.service.ts
+++ server/services/file.service.ts
@@ -1,7 +1,6 @@
 import { Form } from "multiparty";
 import { upload } from "./image.service";
-const uuid = require("uuid/v4");
-const fs = require("fs");
+const base64Img = require("base64-img");
 
 export const uploadFile = req =>
   new Promise<string>((resolve, reject) => {
@@ -13,16 +12,30 @@ export const uploadFile = req =>
         uploadDir: "public/files",
         maxFilesSize: 1048576 * 3
       });
+
       form.parse(req, function(err, fields, files): any {
         if (err) return reject(err);
+        if (process.env.NODE_ENV === "production") {
+          base64Img.base64(files.file[0].path, function(err, data) {
+            upload(data)
+              .then(url => resolve(url))
+              .catch(e => reject(e));
+          });
+        } else {
+          if (files.file) {
+            let imageUrl = files.file[0].path;
+            let url;
+            url =
+              imageUrl.indexOf("\\") !== -1
+                ? imageUrl.split(`\\`)
+                : imageUrl.split(`/`);
+            url.shift();
+            url = url.join("/");
 
-        const file = Buffer.from(fs.readFileSync(files.file[0].path)).toString(
-          "base64"
-        );
-
-        upload(file)
-          .then(url => resolve(url))
-          .catch(e => reject(e));
+            url = "/" + url;
+            return resolve(url);
+          }
+        }
       });
     } else if (contentType && contentType === "octet-stream") {
       const buffer = [];
