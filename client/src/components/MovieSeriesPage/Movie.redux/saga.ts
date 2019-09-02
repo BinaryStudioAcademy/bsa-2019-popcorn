18e101947301db73e528af69e0203831e9193c33
igor185 <igor.babin185@gmail.com>
2019-09-02 20:54:33 +0300

    movie-list: fix movie adapter parser

--- client/src/components/MovieSeriesPage/Movie.redux/saga.ts
+++ client/src/components/MovieSeriesPage/Movie.redux/saga.ts
@@ -3,39 +3,37 @@ import {
 	FINISH_FETCH_SEARCH_FILMS,
 	START_FETCH_SEARCH_FILMS
 } from '../../shared/Header/actionTypes';
-import { START_SEARCH_ELASTIC_FILMS } from '../../UserPage/UserTops/UserTops.redux/actionTypes';
 import webApi from '../../../services/webApi.service';
 import {
+	FETCH_FILTRED_MOVIES,
 	FETCH_MOVIE_BY_ID,
 	FETCH_MOVIE_BY_ID_SUCCESS,
 	FETCH_MOVIE_LIST,
 	FETCH_MOVIE_USER_RATE,
 	FETCH_MOVIE_USER_RATE_SUCCESS,
-	FETCH_SEARCH,
-	FETCH_SEARCH_TO_ADD_MOVIE,
-	LOAD_MORE_MOVIE,
-	LOADING,
-	SET_LOAD_MORE_MOVIE,
-	SET_MOVIE_LIST,
-	SET_SEARCH_MOVIE,
-	SET_SEARCH_MOVIE_TO_ADD,
-	SET_USER_RATE,
 	FETCH_REVIEW_BY_USER_MOVIE_ID,
 	FETCH_REVIEW_BY_USER_MOVIE_ID_SUCCESS,
+	FETCH_SEARCH,
+	FETCH_SEARCH_TO_ADD_MOVIE,
+	GET_AWARDS,
+	GET_GENRES,
+	LOAD_MORE_FILTRED_MOVIE,
+	LOAD_MORE_MOVIE,
+	LOADING,
+	SET_AWARDS,
+	SET_FILTRED_MOVIE_LIST,
+	SET_GENRES,
+	SET_HIDE_SPINNER,
+	SET_LOAD_MORE_FILTRED_MOVIE,
+	SET_LOAD_MORE_MOVIE,
+	SET_MOVIE_LIST,
 	SET_REVIEW,
 	SET_REVIEW_SUCCESS,
-	SET_AWARDS,
-	GET_AWARDS,
-	FETCH_FILTRED_MOVIES,
-	SET_FILTRED_MOVIE_LIST,
-	SET_LOAD_MORE_FILTRED_MOVIE,
-	LOAD_MORE_FILTRED_MOVIE,
+	SET_SEARCH_MOVIE,
+	SET_SEARCH_MOVIE_TO_ADD,
 	SET_SHOW_SPINNER,
-	SET_HIDE_SPINNER,
-	GET_GENRES,
-	SET_GENRES
+	SET_USER_RATE
 } from './actionTypes';
-import config from '../../../config';
 import { FETCH_MOVIE_REVIEWS } from '../MovieSeriesReviews/actionTypes';
 
 export function* fetchFilms(action) {
@@ -111,10 +109,14 @@ export function* fetchAwards(action) {
 
 export function* fetchMovieList() {
 	try {
-		const data = yield call(webApi, {
-			endpoint: '/api/movie',
-			method: 'GET'
+		let data = yield call(webApi, {
+			endpoint: `/api/movie`,
+			method: 'GET',
+			parse: false
 		});
+
+		data = yield call(data.json.bind(data));
+
 		yield put({
 			type: SET_MOVIE_LIST,
 			payload: {
@@ -122,7 +124,7 @@ export function* fetchMovieList() {
 			}
 		});
 	} catch (e) {
-		console.log('movie saga fetchMovieList:', e.message);
+		console.log('movie saga fetchMovieList:', e);
 	}
 }
 
@@ -248,12 +250,15 @@ export function* fetchSearchMovie(action) {
 
 export function* loadMoreMovie(action) {
 	const { size, from } = action.payload;
-	console.log('hidden1');
 	try {
-		const data = yield call(webApi, {
+		let data = yield call(webApi, {
 			endpoint: `/api/movie?from=${from}&size=${size}`,
-			method: 'GET'
+			method: 'GET',
+			parse: false
 		});
+
+		data = yield data.json();
+
 		yield put({
 			type: SET_LOAD_MORE_MOVIE,
 			payload: {
