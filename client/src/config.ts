f1fd1a3b9a614186bd7c148aeb9182385c5c5c36
Kirill Burmelov <burik0304@icloud.com>
2019-09-03 01:45:40 +0300

    movie-list: add default images to config

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,6 +4,7 @@ const config = {
 	DEFAULT_MOVIE_IMAGE: '/images/default/movie-default.png',
 	DEFAULT_TOP_IMAGE:
 		'https://www.goldderby.com/wp-content/uploads/2017/12/Oscar-statuette-trophy-atmo.png',
+	DEFAULT_MOVIELIST_IMAGE: '/images/default/movielist-default.png',
 	POSTER_PATH: 'https://image.tmdb.org/t/p/w500'
 };
 const firebaseConfig = {
