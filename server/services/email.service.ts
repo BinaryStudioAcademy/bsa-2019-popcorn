dca40ccd40b94a7f3b143bce56014f358a30e805
Roman Miniv <1pi15bminivroman@gmail.com>
2019-09-02 16:48:44 +0300

    feature/send-welcome-mail: changed styles

--- server/services/email.service.ts
+++ server/services/email.service.ts
@@ -53,9 +53,112 @@ export const sendWelcomeEmail = (email) => {
     from: process.env.USER_MAIL,
     subject: "Congratulation with registation",
     html: `
-      <h1 style='text-align: center;'>
-        Welcome to Pop Corn!
-      <h1>
+      <div
+        style='
+          width: 40%;
+          margin: 0 auto;
+        '  
+      >
+        <header
+          style='
+            padding: 10px 5px;
+            box-sizing: border-box;
+            border-top: 4px solid silver;
+            border-bottom: 4px solid silver;
+          '
+        >
+          <a
+            href=${process.env.FRONTEND_HOST}
+            style='
+              display: flex;
+              justify-conent: left;
+              align-items: center;
+              text-decoration: none;
+              color: #000;
+            '
+          >
+            <img
+              src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Anime_eye.svg/2000px-Anime_eye.svg.png'
+              alt='popcorn-logo'
+              style='
+                width: 50px;
+                height: 50px;
+              '
+            />
+            <h2
+              style='
+                margin-left: 5px;
+              '>
+            Pop Corn</h2>
+          </a>
+
+          
+        </header>
+        
+        <main
+          style='
+            font-size: 16px;
+          '
+        >
+          <h1
+            style='color: #000'
+          >Welcome to Pop Corn!</h1>
+          <p
+            style='
+              font-size: 16px;
+              color: #000;
+            '
+          >
+            Please, go to <a href='${process.env.FRONTEND_HOST}'>link</a> for confirm registration.
+          </p>
+
+          <p
+            style='
+              font-size: 16px;
+              color: #000;
+            '
+          >You might be interested in:</p>
+          <ul
+            style='
+              font-size: 16px;
+              color: #000;
+              cursor: pointer;
+            '
+          >
+            <li><a href='${process.env.FRONTEND_HOST}/tops'>Tops</a></li>
+            <li><a href='${process.env.FRONTEND_HOST}/events'>Events</a></li>
+            <li><a href='${process.env.FRONTEND_HOST}/surveys'>Surveys</a></li>
+          </ul>
+
+          <a href='${process.env.FRONTEND_HOST}' style='cursor: pointer;'>
+            <button 
+              style='
+                box-sizing: border-box;
+                padding: 10px 20px;  
+                border: none;
+                border-radius: 15px;
+                outline: none;
+                background-color: #ff6501;
+                text-align: center;
+                letter-spacing: 0.4px;
+                line-height: 22px;
+                font-size: 18px;
+                font-weight: 600;
+                color: #fff;
+              '
+            >Let's go</button>
+          </a>
+        </main>
+        
+        <footer
+          style='
+            font-size: 14px;
+            color: #000;
+          '
+        >
+          <p>The Pop Corn Team</p>
+        </footer>
+      </div>
     `
   };
   return emailRepository.send(msg); 
