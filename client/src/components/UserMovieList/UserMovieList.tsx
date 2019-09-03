f2cf1d4bd1e2179e0b4d07a740a0951937110056
Kirill Burmelov <burik0304@icloud.com>
2019-09-03 23:14:38 +0300

    movie-list: create header for UserMovieList component

--- client/src/components/UserMovieList/UserMovieList.tsx
+++ client/src/components/UserMovieList/UserMovieList.tsx
@@ -1,24 +1,118 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { fetchMovieListDetails } from '../UserPage/UserLists/actions';
+import Spinner from '../shared/Spinner';
+import './UserMovieList.scss';
+import Image from '../shared/Image/Image';
+import config from '../../config';
+import { NavLink } from 'react-router-dom';
+import ReactTimeAgo from 'react-time-ago';
 
 interface IProps {
 	match: any;
-	currentUserId: string;
+	fetchMovieListDetails: (movieListId: string) => object;
+	isLoading: boolean;
+	movieListDetails: IMovieListDetails;
+}
+
+interface IMovieListDetails {
+	movieList: {
+		id: string;
+		title: string;
+		description: string;
+		isPrivate: boolean;
+		image_url: string;
+		moviesId: Array<string>;
+		created_at: Date;
+		user: {
+			id: string;
+			name: string;
+			avatar: string;
+		};
+	};
+	movies: Array<IMovie>;
+}
+
+interface IMovie {
+	id: string;
+	title: string;
+	poster_path: string;
+	release_date: string;
+	runtime: number;
 }
 
 const UserMovieList: React.FC<IProps> = ({ ...props }) => {
-	return <div className="UserMovieList">MovieList</div>;
+	const {
+		match: {
+			params: { id: movieListId }
+		},
+		isLoading,
+		movieListDetails,
+		fetchMovieListDetails
+	} = props;
+
+	if (!movieListDetails || movieListDetails.movieList.id !== movieListId) {
+		fetchMovieListDetails(movieListId);
+		return <Spinner />;
+	}
+
+	const {
+		id,
+		title,
+		description,
+		isPrivate,
+		image_url,
+		created_at,
+		user: { id: userId, name: userName, avatar: userAvatar }
+	} = movieListDetails.movieList;
+
+	return (
+		<div className="UserMovieList">
+			<div className="movie-list-container">
+				<header className="movie-list-header">
+					<div className="movie-list-image-container">
+						<Image
+							src={image_url}
+							defaultSrc={config.DEFAULT_MOVIELIST_IMAGE}
+							alt="movie-list"
+						/>
+					</div>
+					<div className="movie-list-main">
+						<div className="movie-list-title">{title}</div>
+						<div className="movie-list-description">{description}</div>
+						<NavLink
+							className="user-info-container"
+							to={`/user-page/${userId}`}
+						>
+							<div className="user-image-container">
+								<Image
+									src={userAvatar}
+									defaultSrc={config.DEFAULT_AVATAR}
+									alt="avatar"
+								/>
+							</div>
+							<div className="user-name">{userName}</div>
+						</NavLink>
+						<div className="movie-list-created">
+							<ReactTimeAgo date={new Date(created_at)} />
+						</div>
+					</div>
+				</header>
+			</div>
+		</div>
+	);
 };
 
 const mapStateToProps = (rootState, props) => ({
-	...props
-	// movieList
+	...props,
+	isLoading: rootState.movieList.isLoading,
+	movieListDetails: rootState.movieList.movieListDetails
 });
 
 const mapDispatchToProps = dispatch => {
 	const actions = {
-		//fetchMovieList
+		fetchMovieListDetails
 	};
 	return bindActionCreators(actions, dispatch);
 };
