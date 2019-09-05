ab1a3fd70903d250be4c35c0fde697978d7f36bd
Taras Nazar <32059567+nazartaras@users.noreply.github.com>
2019-09-06 01:43:44 +0300

    Update actions.ts

--- client/src/components/MainPage/RecommendList/RecommendList.redux/actions.ts
+++ client/src/components/MainPage/RecommendList/RecommendList.redux/actions.ts
@@ -1,7 +1,6 @@
 import { FETCH_RECOMMENDED } from './actionTypes';
 
 export const fetchRecommended = (userId: string) => {
-	console.log('hello');
 	return {
 		type: FETCH_RECOMMENDED,
 		payload: userId
