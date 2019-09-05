e29d357089026ca67bc61c235f7e1bd4e4392abd
prikhodkoos <prikhodks@gmail.com>
2019-09-05 23:29:17 +0300

    chat: provided navlinks to user page

--- client/src/components/ChatPage/Chat/ChatHeader.tsx
+++ client/src/components/ChatPage/Chat/ChatHeader.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 interface IProps {
 	user: {
@@ -12,7 +13,9 @@ const ChatHeader: React.FC<IProps> = ({ user }) => {
 	return (
 		<div className="chat-header">
 			<img className="header-avatar" src={user.avatar} alt="avatar" />
-			<div className="user-name">{user.name}</div>
+			<NavLink to={`/user-page/${user.id}`}>
+				<div className="user-name">{user.name}</div>
+			</NavLink>
 		</div>
 	);
 };
