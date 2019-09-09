74112e2b4216efb5bf23f037d2b54bf94b6106ba
Taras <tarasnazar93@gmail.com>
2019-09-09 10:30:06 +0300

    fix/header-avatar: fix avatar update

--- client/src/components/shared/Header/Header.tsx
+++ client/src/components/shared/Header/Header.tsx
@@ -101,9 +101,6 @@ class Header extends React.Component<IProps> {
 		const PROFILE = 'Profile';
 		const SETTINGS = 'Settings';
 		const LOGOUT = 'Logout';
-
-		const { avatar } = userInfo;
-
 		return (
 			<div className="header">
 				<NavLink to="/" className="header-logo-link">
@@ -196,7 +193,10 @@ class Header extends React.Component<IProps> {
 					}
 				</div>
 				<div className="user-info header-buttons hover">
-					<Image src={avatar} defaultSrc={config.DEFAULT_AVATAR} alt="avatar" />
+					<img
+						src={userInfo.avatar ? userInfo.avatar : config.DEFAULT_AVATAR}
+						alt="avatar"
+					/>
 					<span className="user-name">{userInfo.name}</span>
 					<div className="modal">
 						<Link
