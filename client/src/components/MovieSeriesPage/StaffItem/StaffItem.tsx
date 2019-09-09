f78c543f153f392cfb17b22e3a73fdcf000a7136
Kirill Burmelov <burik0304@icloud.com>
2019-09-09 17:28:24 +0300

    crew-cast: add default image

--- client/src/components/MovieSeriesPage/StaffItem/StaffItem.tsx
+++ client/src/components/MovieSeriesPage/StaffItem/StaffItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './StaffItem.scss';
 import config from '../../../config';
 import { StaffItemInfo } from '../StaffList/StaffType';
+import Image from '../../shared/Image/Image';
 
 type StaffItemProps = {
 	staffItemInfo: StaffItemInfo;
@@ -13,13 +14,11 @@ const StaffItem = ({
 	return (
 		<div className="staff-item">
 			<div className={'staff-item-image-wrp'}>
-				<img
+				<Image
+					src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${profile_path}`}
+					defaultSrc={config.DEFAULT_CREW_CAST_IMAGE}
 					className="staff-item-image"
-					src={
-						profile_path
-							? `https://image.tmdb.org/t/p/w600_and_h900_bestv2/${profile_path}`
-							: config.DEFAULT_AVATAR
-					}
+					alt="staff photo"
 				/>
 			</div>
 			<div className="staff-item-name">{name}</div>
