9db1bdf1aeb79bc1f9fe93313d28653f247a02e7
igor185 <igor.babin185@gmail.com>
2019-09-10 08:05:59 +0300

    delete console.log

--- server/controllers/voting.controller.ts
+++ server/controllers/voting.controller.ts
@@ -59,7 +59,6 @@ router
       votingOptionService
         .setVotingReaction(req.params.votingId, req.body, next) // req.body: {userId: string, optionId}
         .then(voting => {
-          console.log(voting);
           req.io.emit("new-voting-reaction", voting);
           res.send(voting);
         })
