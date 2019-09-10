d01f7c36470a84211fe9df9d9cb4220ae2e59c0a
Kirill Burmelov <burik0304@icloud.com>
2019-09-10 23:33:18 +0300

    advice-me: add movie rate component

--- client/src/components/shared/AdviceMe/MovieCardItem/AdviceMeItem.tsx
+++ client/src/components/shared/AdviceMe/MovieCardItem/AdviceMeItem.tsx
@@ -4,6 +4,8 @@ import config from '../../../../config';
 import Image from '../../Image/Image';
 import TMovie from '../../../MovieSeriesPage/TMovie';
 import WatchListIcon from '../../WatchListIcon/WatchListIcon';
+import { NavLink } from 'react-router-dom';
+import RateMovie from '../../RateMovie/RateMovie';
 
 interface IProps {
 	movie: TMovie;
@@ -16,18 +18,29 @@ const MovieCardItem: React.FC<IProps> = ({ movie }) => {
 		mainCast,
 		video,
 		poster_path,
-		title
+		title,
+		runtime,
+		genres
 	} = movie;
 
 	return (
 		<div className="AdviceMeItem">
 			<div className="advice-me-header">
-				<WatchListIcon movieid={movieId} />
-				<div className="header-center">
-					<div className="advice-me-title">
+				<div className="movie-item-title-rating">
+					<NavLink to={`/movies/${movieId}`} className="movie-item-title">
 						{title}
-						<span className="advice-me-year"></span>
+						<span className="movie-item-date">
+							{release_date ? ' (' + release_date.slice(0, 4) + ')' : null}
+						</span>
+					</NavLink>
+					<div className="movie-item-rating">
+						<RateMovie movieId={movieId} />
 					</div>
+				</div>
+				<div className="movie-item-duration-genres">
+					{runtime && <span className="duration">{`${runtime} min`}</span>}
+					<span className="border-element">{' | '}</span>
+					<span className="genres">{genres}</span>
 				</div>
 			</div>
 		</div>
