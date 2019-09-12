105b45dbc7b9aff2c298e303014fc9ce4ff11628
Kirill Burmelov <burik0304@icloud.com>
2019-09-12 17:58:20 +0300

    movie-rate: create function for get total movie rate

--- client/src/helpers/getTotalMovieRate.ts
+++ client/src/helpers/getTotalMovieRate.ts
@@ -1,11 +1,16 @@
 export default (rateInfo: any, prevUserRate: any, userRate: any) => {
 	const prevSumOfRates = rateInfo.count * rateInfo.average;
-	const newSumOfRates = prevSumOfRates - (prevUserRate || 0) + (userRate || 0);
+	const newSumOfRates = prevUserRate
+		? prevSumOfRates - prevUserRate + (userRate || 0)
+		: prevSumOfRates + userRate;
 	if (!prevUserRate && userRate) {
-		rateInfo.count = rateInfo.count + 1;
+		rateInfo.count = +rateInfo.count + 1;
 	} else if (prevUserRate && !userRate) {
 		rateInfo.count = rateInfo.count - 1;
 	}
-	rateInfo.average = (newSumOfRates / rateInfo.count).toFixed(1);
+	rateInfo.average = +rateInfo.count
+		? (newSumOfRates / rateInfo.count).toFixed(2)
+		: 0;
+
 	return rateInfo;
 };
